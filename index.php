80fc5c5024688b3e3c6b49e82038f7ad3e5368f6
Yaman Adamnor <yamada091@edu.linkoping.se>
2019-03-12 13:52:12 +0000

    added weather_data to array and converted data to UTF-8 from UTF-16
    Extracted data from weather_data.csv to array and converted the data to UTF-8 from UTF-16

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
-<? php
-var_dump($_GET);
+<?php
+
+
+$filename = "weather_data.csv";
+
+// The nested array that holds the other arrays 
+$weatherDataArr = [];
+
+
+// Open the file for reading
+if (($h = fopen($filename, "r")) !== FALSE) 
+{
+  // Convert each line into the local $data variable
+  while (($data = fgetcsv($h, 1000, ",")) !== FALSE) 
+  {		
+      $weatherDataArr[] = $data;
+  }
+
+  // Close the file
+  fclose($h);
+}
+
+// Converts every array object to UTF-8 from UTF-16
+for ($i = 0; $i < count($weatherDataArr); $i++) {
+  $results[$i] = mb_convert_encoding($weatherDataArr[$i], "UTF-8", "UTF-16");
+}
+
+
+var_dump($results);
